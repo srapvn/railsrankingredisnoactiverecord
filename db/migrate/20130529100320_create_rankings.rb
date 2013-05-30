1d05e2de98896e88f798ec446380423c16ee778d
Khanh Nguyen <lkknguyen@gmail.com>
2013-05-30 15:14:05 +0200

    init

--- db/migrate/20130529100320_create_rankings.rb
+++ db/migrate/20130529100320_create_rankings.rb
@@ -3,11 +3,11 @@ class CreateRankings < ActiveRecord::Migration
     create_table :rankings do |t|
       t.integer :userid, :null => false
       t.string :name, :null => false
-      t.float :winpercentage
-      t.integer :level
-      t.integer :xp
-      t.string :honor
-      t.integer :military
+      t.float :winpercentage,  default: 0
+      t.integer :level,  default: 0
+      t.integer :xp,  default: 0
+      t.integer :honor,  default: 0
+      t.integer :military,  default: 0
 
       t.timestamps
     end
