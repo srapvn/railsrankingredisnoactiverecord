e825d791789203b2504f4c6f170ba227763d7a29
Khanh Nguyen <lkknguyen@gmail.com>
2013-06-01 19:20:53 +0200

    2

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,6 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-unless $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-   $redis = Redis.new(:host => 'localhost', :port => 6379)
-end
+# uri = URI.parse(ENV["REDISTOGO_URL"])
+# unless $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+#    $redis = Redis.new(:host => 'localhost', :port => 6379)
+# end
+
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
