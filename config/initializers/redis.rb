7316c30a467dd68af8f4107b2281d7f6cafd81fb
Khanh Nguyen <lkknguyen@gmail.com>
2013-06-01 19:39:43 +0200

    2

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,7 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
+# uri = URI.parse(ENV["REDISTOGO_URL"])
 # unless $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 #    $redis = Redis.new(:host => 'localhost', :port => 6379)
 # end
 
-$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+$redis = Redis.new(:host => 'squawfish.redistogo.com', :port => 10098, :password => '712e69263ebaaa8e314d11266c96639e')
+
